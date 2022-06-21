1e314264516e07eb0f1bad0d8d4b871f03b79b52
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-06-22 05:20:08 +0700

    HaiZuka first commit

---
+++ src/main/java/com/linh/HomeServlet.java
@@ -0,0 +1,56 @@
+package com.linh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet(urlPatterns = "/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+//		response.getWriter().append("Hello: ").append(request.getContextPath());
+//		String name = request.getParameter("name");
+//		String message = "Minh la " + name;
+//		request.setAttribute("message", message);
+//		requestDispatcher.forward(request, response);
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		try(PrintWriter out = resp.getWriter()) {
+			out.print("Hello");
+		}
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>MLink</title>
+</head>
+<body>
+	<h1>Hello Linh</h1>
+	<h1>${message }</h1>
+</body>
+</html>
