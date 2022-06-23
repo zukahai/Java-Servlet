69ad9efcbe495de8a47d0a285094499922fff054
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-06-23 21:45:38 +0700

    40%

---
+++ src/main/java/com/hai/utils/JpaUtils.java
@@ -0,0 +1,12 @@
+package com.hai.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtils {
+	public static EntityManager getEntityManager() {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Servlet");
+		return entityManagerFactory.createEntityManager();
+	}
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String username;
+
+	private String password;
+	
+	private String fullname;
+	
+	private int age;
+
+	public User() {
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
